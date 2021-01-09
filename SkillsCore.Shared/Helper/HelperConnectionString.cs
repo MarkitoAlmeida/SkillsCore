626ebc590392955a4b3485402cdb84290de34b38
Marcos Almeida <mpalmeida88@gmail.com>
2021-01-09 16:49:09 +0000

    Removendo ConnectionString.

--- SkillsCore.Shared/Helper/HelperConnectionString.cs
+++ SkillsCore.Shared/Helper/HelperConnectionString.cs
@@ -2,6 +2,6 @@
 {
     public class HelperConnectionString
     {
-        public static string connectionString = "Data Source=pp-si-bd01.parfois.com:1521/ONLPP;User ID=ONLINEAPP;Password=LrYFc5bp";
+        public static string connectionString = "";
     }
 }
